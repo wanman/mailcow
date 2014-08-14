28ce6ea8d13c86ea4aa7fb5a47512a93fad5d1fe
andryyy <apeters87@gmail.com>
2014-08-14 15:46:33 +0200

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ genpasswd() {
       	tr -cd '[:alnum:]' < /dev/urandom | fold -w${l} | head -n1
 }
 
-[[ ! -z `ss -lnt | awk '$1 == "LISTEN" && $4 ~ ":25" || $4 ~ ":143" || $4 ~ ":993" || $4 ~ ":587" || $4 ~ ":485" || $4 ~ ":80" || $4 ~ ":443" || $4 ~ ":995"'` ]] && echo "please remove any mail and web services before running this script"; exit 1;
+[[ ! -z `ss -lnt | awk '$1 == "LISTEN" && $4 ~ ":25" || $4 ~ ":143" || $4 ~ ":993" || $4 ~ ":587" || $4 ~ ":485" || $4 ~ ":80" || $4 ~ ":443" || $4 ~ ":995"'` ]] && { echo "please remove any mail and web services before running this script"; exit 1; }
 
 ########### CONFIG START ###########
 sys_hostname="mail"
@@ -24,7 +24,7 @@ echo MySQL root password: $my_rootpw >> installer.log
 echo ---------- >> installer.log
 
 # set hostname
-sethostname {
+function sethostname {
 cat > hosts<<'EOF'
 127.0.0.1 localhost
 ::1 localhost ip6-localhost ip6-loopback
@@ -38,30 +38,30 @@ service hostname.sh start
 }
 
 # installation
-installpackages {
+function installpackages {
 DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install python-sqlalchemy python-beautifulsoup python-setuptools \
 python-magic openssl php-auth-sasl php-http-request php-mail php-mail-mime php-mail-mimedecode php-net-dime php-net-smtp \
-php-net-socket php-net-url php-pear php-soap php5 php5-cli php5-common php5-curl php5-fpm php5-gd php5-imap svn \
+php-net-socket php-net-url php-pear php-soap php5 php5-cli php5-common php5-curl php5-fpm php5-gd php5-imap subversion \
 php5-intl php5-mcrypt php5-mysql php5-sqlite mysql-client mysql-server nginx dovecot-common dovecot-core mailutils \
 dovecot-imapd dovecot-lmtpd dovecot-managesieved dovecot-sieve dovecot-mysql dovecot-pop3d postfix \
 postfix-mysql postfix-pcre clamav clamav-base clamav-daemon clamav-freshclam spamassassin fail2ban
 }
 
 # certificate
-selfsignedcert {
+function selfsignedcert {
 mkdir /etc/ssl/mail
 openssl req -new -newkey rsa:4096 -days 1095 -nodes -x509 -subj "/C=DE/ST=SESI/L=SESI/O=SESI/CN=$sys_hostname.$sys_domain" -keyout /etc/ssl/mail/mail.key  -out /etc/ssl/mail/mail.crt
 chmod 600 /etc/ssl/mail/mail.key
 }
 
 # mysql
-mysqlconfiguration {
+function mysqlconfiguration {
 mysqladmin -u root password $my_rootpw
 mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE $my_postfixdb; CREATE USER '$my_postfixuser'@'localhost' IDENTIFIED BY '$my_postfixpass'; GRANT ALL PRIVILEGES ON `$my_postfixdb` . * TO '$my_postfixuser'@'localhost';"
 }
 
 # fuglu
-fuglusetup {
+function fuglusetup {
 mkdir /var/log/fuglu
 rm /tmp/fuglu_control.sock
 chown nobody:nogroup /var/log/fuglu
@@ -77,7 +77,7 @@ update-rc.d fuglu defaults
 }
 
 # postfix
-postfixconfig {
+function postfixconfig {
 cp -R postfix/* /etc/postfix2/
 chown root:postfix "/etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf"; chmod 640 "/etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf"
 chown root:postfix "/etc/postfix/sql/mysql_virtual_alias_maps.cf"; chmod 640 "/etc/postfix/sql/mysql_virtual_alias_maps.cf"
@@ -97,7 +97,7 @@ sed -i "s/my_postfixdb/$my_postfixdb/g" /etc/postfix/sql/*
 }
 
 # dovecot
-dovecotconfig {
+function dovecotconfig {
 chown root:dovecot "/etc/dovecot/dovecot-dict-sql.conf"; chmod 640 "/etc/dovecot/dovecot-dict-sql.conf"
 chown root:vmail "/etc/dovecot/dovecot-mysql.conf"; chmod 640 "/etc/dovecot/dovecot-mysql.conf"
 chown root:root "/etc/dovecot/dovecot.conf"; chmod 644 "/etc/dovecot/dovecot.conf"
@@ -115,7 +115,7 @@ chown -R vmail:vmail /var/vmail
 }
 
 # clamv
-clamavconfig {
+function clamavconfig {
 service clamav-daemon stop
 service clamav-freshclam stop
 freshclam
@@ -126,7 +126,7 @@ service clamav-daemon start
 }
 
 # spamassassin
-spamassassinconfig {
+function spamassassinconfig {
 sed -i '/rewrite_header/c\rewrite_header Subject [SPAM]' /etc/spamassassin/local.cf
 sed -i '/report_safe/c\report_safe 2' /etc/spamassassin/local.cf
 sed -i '/^OPTIONS=/s/=.*/="--create-prefs --max-children 5 --helper-home-dir --username debian-spamd"/' /etc/default/spamassassin
@@ -135,7 +135,7 @@ sed -i '/^ENABLED=/s/=.*/="1"/' /etc/default/spamassassin
 }
 
 # nginx, php5
-websrvconfig {
+function websrvconfig {
 rm -rf /etc/php5/fpm/pool.d/*
 rm -rf /etc/nginx/{sites-available,sites-enabled}/*
 cp misc/mail_nginx /etc/nginx/sites-available/
@@ -144,7 +144,7 @@ sed -i '/server_tokens/c\server_tokens off;' /etc/nginx/nginx.conf
 }
 
 # pfadmin
-pfadminconfig {
+function pfadminconfig {
 mkdir /usr/share/nginx/mail
 svn co http://svn.code.sf.net/p/postfixadmin/code/trunk /usr/share/nginx/mail/pfadmin
 cp misc/config.local.php_pfadmin /usr/share/nginx/mail/pfadmin/config.local.php
@@ -157,18 +157,18 @@ chown -R www-data: /usr/share/nginx/
 }
 
 # fail2ban
-fail2banconfig {
+function fail2banconfig {
 cp misc/jail.local_fail2ban /etc/fail2ban/jail.local
 cp misc/sasl.conf_fail2ban /etc/fail2ban/filter.d/sasl.conf
 cp misc/dovecot-pop3imap.conf_fail2ban /etc/fail2ban/dovecot-pop3imap.conf
 }
 
 # rsyslogd
-rsyslogd {
+function rsyslogd {
 sed "s/*.*;auth,authpriv.none/*.*;auth,mail.none,authpriv.none/" -i /etc/rsyslog.conf
 }
 
-restartservices {
+function restartservices {
 service fail2ban restart
 service nginx restart
 service fail2ban restart
