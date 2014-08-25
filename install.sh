b3dbab38fc510d92fd538b9d95ac638c1a78a062
andryyy <apeters87@gmail.com>
2014-08-25 13:36:04 +0200

    socket for SA, add fuglu to group debian-spamd

--- install.sh
+++ install.sh
@@ -142,6 +142,7 @@ function fuglusetup {
 mkdir /var/log/fuglu 2> /dev/null
 groupadd fuglu
 useradd -g fuglu -s /bin/false fuglu
+usermod -a -G debian-spamd fuglu
 usermod -a -G clamav fuglu
 rm /tmp/fuglu_control.sock 2> /dev/null
 chown fuglu:fuglu /var/log/fuglu
@@ -216,7 +217,7 @@ service clamav-daemon start
 function spamassassinconfig {
 sed -i '/rewrite_header/c\rewrite_header Subject [SPAM]' /etc/spamassassin/local.cf
 sed -i '/report_safe/c\report_safe 2' /etc/spamassassin/local.cf
-sed -i '/^OPTIONS=/s/=.*/="--create-prefs --max-children 5 --helper-home-dir --username debian-spamd"/' /etc/default/spamassassin
+sed -i '/^OPTIONS=/s/=.*/="--create-prefs --max-children 5 --helper-home-dir --username debian-spamd --socketpath /var/run/spamd.sock --socketowner debian-spamd --socketgroup debian-spamd"/' /etc/default/spamassassin
 sed -i '/^CRON=/s/=.*/="1"/' /etc/default/spamassassin
 sed -i '/^ENABLED=/s/=.*/="1"/' /etc/default/spamassassin
 }
