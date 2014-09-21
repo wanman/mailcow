4467a3dc87dd2d4daec9cf64b519c802f36a73ec
andryyy <apeters87@gmail.com>
2014-09-21 09:48:01 +0200

    port checking function changed, fixed smtps port

--- install.sh
+++ install.sh
@@ -71,7 +71,16 @@ cert_org="MAIL"
 #### do not edit any line below ####
 ####################################
 
-[[ ! -z `ss -lnt | awk '$1 == "LISTEN" && $4 ~ ":25" || $4 ~ ":143" || $4 ~ ":993" || $4 ~ ":587" || $4 ~ ":485" || $4 ~ ":80" || $4 ~ ":443" || $4 ~ ":995"'` ]] && { echo "`tput setaf 1``tput bold`Please remove any mail and web services before running this script.`tput sgr0`"; echo ; echo ; echo "netstat -tulpen" ; echo "---------------" ; echo ; netstat -tulpen ; exit 1; }
+CHECKPORTS=$(ss -lnt | awk '$1 == "LISTEN" && $4 ~ ":25" || $4 ~ ":143" || $4 ~ ":993" || $4 ~ ":587" || $4 ~ ":465" || $4 ~ ":80" || $4 ~ ":443" || $4 ~ ":995"')
+if [[ ! -z $CHECKPORTS ]]; then
+echo "`tput setaf 1``tput bold`Please remove any mail and web services before running this script.`tput sgr0`"
+echo "Possible ports blocking the installation: 25/TCP, 143/TCP, 993/TCP, 587/TCP, 465/TCP, 80/TCP, 443/TCP, 995/TCP"
+echo
+echo "Here is a list of currently listening services"
+echo "----------------------------------------------"
+netstat -tulpen
+exit 1
+fi
 
 function returnwait {
 echo "`tput setaf 4``tput bold`$1`tput sgr0` - `tput setaf 2``tput bold`[OK]`tput sgr0`";
