9463660dec8ac9af4ef5d64057269b917956a41a
andryyy <apeters87@gmail.com>
2015-01-19 09:39:09 +0100

    Do not show DAV settings on dashboard when Radicale binary is not found

--- nginx/conf/htdocs/index.php
+++ nginx/conf/htdocs/index.php
@@ -41,7 +41,9 @@ a:active,a:hover,a:link,a:visited{color:inherit;text-decoration:none;outline: 0;
   <li>Port 587 STARTTLS</li>
 </ul>
 </p>
-
+<?php
+if (file_exists("/usr/local/bin/radicale")) {
+?>
 <div class="line"></div>
 <h3>Cal- and CardDAV client settings</h3>
 <p><b>CalDAV access</b>
@@ -54,7 +56,7 @@ a:active,a:hover,a:link,a:visited{color:inherit;text-decoration:none;outline: 0;
 </ul>
 * the trailing slash is required.
 </p>
-
+<?php } ?>
 <div class="line"></div>
 
 <h3>Health check</h3>
