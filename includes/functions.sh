e3ed9c19457739fe4d00ad1570ebb8a1852fb18d
andryyy <apeters87@gmail.com>
2015-01-20 07:20:07 +0100

    Install whiptail at environment check to satisfy dpkg

--- includes/functions.sh
+++ includes/functions.sh
@@ -150,6 +150,10 @@ EOF
 				echo "$(redb [ERR]) - Cannot set your hostname!"
 				exit 1
 			fi
+			if [[ -z $(which whiptail) ]]; then
+				echo "$(textb [INFO]) - Installing whiptail as dialog frontend to satisfy dpkg..."
+				apt-get -y update > /dev/null ; apt-get -y install whiptail > /dev/null 2>&1
+			fi
 			if [[ -f /lib/systemd/systemd ]]; then
 				echo "$(textb [INFO]) - Checking for dbus, this may take a few seconds..."
 				apt-get -y update > /dev/null ; apt-get -y install dbus > /dev/null
