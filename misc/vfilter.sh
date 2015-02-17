7c2c436351051f14a564be7f14a7ee806fb9c3f4
andryyy <apeters87@gmail.com>
2015-02-17 14:20:49 +0100

    vfilter multi recipient fix

--- misc/vfilter.sh
+++ misc/vfilter.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 SENDMAIL="/usr/sbin/sendmail -G -i"
 WORKDIR="/var/vmail/vfilter"
-TIME=$(date +%s)
 APIKEY=$(cat /var/www/VT_API_KEY)
+RAND=$(echo $RANDOM)
 
-rm -r /tmp/message*
-rm -rf "$WORKDIR/scandir"
 cat > /tmp/message.$$
-mkdir -p "$WORKDIR/scandir/$4/$TIME" 2> /dev/null
-/usr/bin/munpack -fqt -C "$WORKDIR/scandir/$4/$TIME" < /tmp/message.$$
-/bin/rm "$WORKDIR/scandir/$4/$TIME"/part*
+
+mkdir -p "$WORKDIR/scandir/$RAND" 2> /dev/null
+
+/usr/bin/munpack -fqt -C "$WORKDIR/scandir/$RAND" < /tmp/message.$$
+/bin/rm "$WORKDIR/scandir/$RAND"/part*
+
 subject=$(cat /tmp/message.$$ | sed -n -e 's/^.*Subject: //p')
 
-for file in $(ls "$WORKDIR/scandir/$4/$TIME/"); do
-        RP="$WORKDIR/scandir/$4/$TIME/$file"
-        vt_hash=$(/usr/bin/curl -X POST 'https://www.virustotal.com/vtapi/v2/file/scan' --form apikey=$APIKEY --form file=@"$RP")
-        echo $vt_hash | /usr/bin/python -mjson.tool | /usr/bin/tr -d '{}",' | /usr/bin/awk 'NF' > /tmp/response.$$
-        /usr/bin/mail -s "Virus scan for \"$file\" in \"$subject\"" "$4" -a "From: VirusTotalUploader" < /tmp/response.$$
+for file in $(ls "$WORKDIR/scandir/$RAND/"); do
+        RP="$WORKDIR/scandir/$RAND/$file"
+        vt_response=$(/usr/bin/curl -X POST 'https://www.virustotal.com/vtapi/v2/file/scan' --form apikey=$APIKEY --form file=@"$RP")
+        echo $vt_response | /usr/bin/python -mjson.tool > /tmp/response.$$
+
+        for each in "${@:4}"; do
+                mail -s "Virus scan for \"$file\" in \"$subject\"" "$each" -a "From: virusservice" < /tmp/response.$$
+        done
+
 done
+
 cat /tmp/message.$$ | $SENDMAIL "$@"
+
+# Cleanup
+rm -r /tmp/message*
+rm -r /tmp/response*
+rm -rf $WORKDIR/scandir/*
