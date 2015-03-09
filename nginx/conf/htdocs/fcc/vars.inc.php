7753f1690a9c780a4b8ebd32913a0ea6c915c176
andryyy <apeters87@gmail.com>
2015-03-09 13:50:31 +0100

    fixes to opendkim implementation

--- nginx/conf/htdocs/fcc/vars.inc.php
+++ nginx/conf/htdocs/fcc/vars.inc.php
@@ -4,5 +4,6 @@ $mailname = str_replace(array("\n", "\r"), '', $mailname);
 $fufix_anonymize_headers = "/etc/postfix/fufix_anonymize_headers.pcre";
 $fufix_reject_attachments = "/etc/postfix/fufix_reject_attachments.regex";
 $fufix_sender_access = "/etc/postfix/fufix_sender_access";
+$fufix_opendkim_dnstxt_folder = "/etc/opendkim/dnstxt";
 $VT_API_KEY = "/var/www/VT_API_KEY";
 ?>
