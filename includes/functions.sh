70b8863da4a8677d158b9cc9da9dfc021e966d25
andryyy <apeters87@gmail.com>
2015-05-01 08:24:03 +0200

    Removed some dependencies

--- includes/functions.sh
+++ includes/functions.sh
@@ -202,8 +202,8 @@ EOF
 				apt-get -y update >/dev/null
 			fi
 			if [[ ! -z $(grep -E "^deb(.*)wheezy-backports(.*)" /etc/apt/sources.list) ]]; then
-				echo "$(textb [INFO]) - Installing jq and python-magic from wheezy-backports..."
-				apt-get -y update >/dev/null ; apt-get -y --force-yes install jq python-magic -t wheezy-backports >/dev/null
+				echo "$(textb [INFO]) - Installing jq from wheezy-backports..."
+				apt-get -y update >/dev/null ; apt-get -y --force-yes install jq -t wheezy-backports >/dev/null
 			fi
             if [[ $conf_httpd == "apache2" ]]; then
 				echo "$(textb [INFO]) - Installing Apache2 and components..."
@@ -222,12 +222,12 @@ EOF
 				echo "$(yellowb [WARN]) - You are running Ubuntu. The installation will not fail, though you may see a lot of output until the installation is finished."
 			fi
 			apt-get -y update >/dev/null
-DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install zip jq dnsutils python-sqlalchemy python-beautifulsoup python-setuptools \
-python-magic libmail-spf-perl libmail-dkim-perl openssl php-auth-sasl php-http-request php-mail php-mail-mime php-mail-mimedecode php-net-dime php-net-smtp \
+DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install zip jq dnsutils python-setuptools libmail-spf-perl libmail-dkim-perl \
+openssl php-auth-sasl php-http-request php-mail php-mail-mime php-mail-mimedecode php-net-dime php-net-smtp \
 php-net-socket php-net-url php-pear php-soap php5 php5-cli php5-common php5-curl php5-fpm php5-gd php5-imap php-apc subversion \
 php5-intl php5-mcrypt php5-mysql php5-sqlite libawl-php php5-xmlrpc mysql-client mysql-server mailutils pyzor razor \
 postfix-mysql postfix-pcre spamassassin spamc sudo bzip2 curl mpack opendkim opendkim-tools unzip clamav-daemon \
-fetchmail liblockfile-simple-perl libdbi-perl libmime-base64-urlsafe-perl libtest-tempdir-perl liblogger-syslog-perl bsd-mailx >/dev/null
+fetchmail liblockfile-simple-perl libdbi-perl libmime-base64-urlsafe-perl libtest-tempdir-perl liblogger-syslog-perl bsd-mailx > /dev/null
 			if [ "$?" -ne "0" ]; then
 				echo "$(redb [ERR]) - Package installation failed"
 				exit 1
