7e8880d42be1aa053f6e49883d4853cfba71fc16
andryyy <andre.peters@debinux.de>
2015-07-29 07:43:49 +0200

    Add CardDAV plugin for RC

--- includes/functions.sh
+++ includes/functions.sh
@@ -443,10 +443,23 @@ DatabaseMirror clamav.inode.at" >> /etc/clamav/freshclam.conf
 			sed -i "s/my_rcdb/$my_rcdb/g" /var/www/mail/rc/config/config.inc.php
 			conf_rcdeskey=$(genpasswd)
 			sed -i "s/conf_rcdeskey/$conf_rcdeskey/g" /var/www/mail/rc/config/config.inc.php
-            sed -i "s/MAILCOW_HOST.MAILCOW_DOMAIN/$sys_hostname.$sys_domain/g" /var/www/mail/rc/config/config.inc.php
+			sed -i "s/MAILCOW_HOST.MAILCOW_DOMAIN/$sys_hostname.$sys_domain/g" /var/www/mail/rc/config/config.inc.php
 			chown -R www-data: /var/www/
 			if [[ $(mysql --defaults-file=/etc/mysql/debian.cnf -s -N -e "use $my_rcdb; show tables;" | wc -l) -lt 5 ]]; then
 				mysql -u $my_rcuser -p$my_rcpass $my_rcdb < /var/www/mail/rc/SQL/mysql.initial.sql
+			fi
+			if [[ ! -d /var/www/mail/rc/plugins/carddav ]]; then
+				wget --quiet https://codeload.github.com/blind-coder/rcmcarddav/zip/master -O /tmp/master.zip && unzip -o /tmp/master.zip -d /var/www/mail/rc/plugins/ > /dev/null
+				mv /var/www/mail/rc/plugins/rcmcarddav-master /var/www/mail/rc/plugins/carddav
+				instcarddav() {
+				cd /var/www/mail/rc/plugins/carddav
+				curl -sS https://getcomposer.org/installer | php
+				php composer.phar install
+				}
+				(instcarddav > /dev/null 2>&1)
+				cp /var/www/mail/rc/plugins/carddav/skins/larry/carddav.css /var/www/mail/rc/skins/larry/
+				mv /var/www/mail/rc/plugins/carddav/{config.inc.php.dist,config.inc.php}
+				mysql --defaults-file=/etc/mysql/debian.cnf ${my_rcdb} < /var/www/mail/rc/plugins/carddav/dbinit/mysql.sql
 			fi
 			rm -rf roundcube/inst/$roundcube_version
 			rm -rf /var/www/mail/rc/installer/
