991480242639f43f8f37a3cbe38f9a2fabb50f9d
andryyy <andre.peters@debinux.de>
2015-07-30 17:13:00 +0200

    Missing dots

--- webserver/htdocs/mail/inc/functions.inc.php
+++ webserver/htdocs/mail/inc/functions.inc.php
@@ -899,9 +899,9 @@ function mailbox_delete_mailbox($link, $postarray) {
 		die("Mail address invalid"); 
 	}
 	$delete_user = "DELETE FROM alias WHERE goto='$username';";
-	$delete_user = "UPDATE alias SET goto=REPLACE(goto, ',$username,', ',');";
-	$delete_user = "UPDATE alias SET goto=REPLACE(goto, ',$username', '');";
-	$delete_user = "UPDATE alias SET goto=REPLACE(goto, '$username,', '');";
+	$delete_user .= "UPDATE alias SET goto=REPLACE(goto, ',$username,', ',');";
+	$delete_user .= "UPDATE alias SET goto=REPLACE(goto, ',$username', '');";
+	$delete_user .= "UPDATE alias SET goto=REPLACE(goto, '$username,', '');";
 	$delete_user .= "DELETE FROM quota2 WHERE username='$username';";
 	$delete_user .= "DELETE FROM calendarobjects WHERE calendarid IN (SELECT id from calendars where principaluri='principals/$username');";
 	$delete_user .= "DELETE FROM cards WHERE addressbookid IN (SELECT id from calendars where principaluri='principals/$username');";
