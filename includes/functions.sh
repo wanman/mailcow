3a3ec0745f3b1a1df2b6c3e6be3bf7a47a46d8d4
andryyy <andre.peters@debinux.de>
2015-09-07 11:35:50 +0200

    Fixes #98

--- includes/functions.sh
+++ includes/functions.sh
@@ -333,6 +333,7 @@ DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install dovecot-common dov
 			groupdel vmail 2> /dev/null
 			groupadd -g 5000 vmail
 			useradd -g vmail -u 5000 vmail -d /var/vmail
+			chmod 755 "/etc/dovecot/"
 			chown root:dovecot "/etc/dovecot/dovecot-dict-sql.conf"; chmod 640 "/etc/dovecot/dovecot-dict-sql.conf"
 			chown root:vmail "/etc/dovecot/dovecot-mysql.conf"; chmod 640 "/etc/dovecot/dovecot-mysql.conf"
 			chown root:root "/etc/dovecot/dovecot.conf"; chmod 644 "/etc/dovecot/dovecot.conf"
