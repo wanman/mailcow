1e068cafea02b1fa6275dc971a1973621bb70e7d
andryyy <andre.peters@debinux.de>
2015-09-10 19:55:52 +0200

    more sh if

--- webserver/htdocs/mail/admin.php
+++ webserver/htdocs/mail/admin.php
@@ -15,11 +15,11 @@ $_SESSION['return_to'] = basename($_SERVER['PHP_SELF']);
 <?php
 $result = mysqli_fetch_assoc(mysqli_query($link, "SELECT username from admin where superadmin='1' and active='1'"));
 ?>
-	<input type="hidden" name="admin_user_now" value="<?php echo $result['username']; ?>">
+	<input type="hidden" name="admin_user_now" value="<?=$result['username'];?>">
 	<div class="form-group">
 		<label class="control-label col-sm-2" for="quota">Administrator:</label>
 		<div class="col-sm-10">
-		<input type="text" class="form-control" name="admin_user" id="admin_user" value="<?php echo $result['username']; ?>" required>
+		<input type="text" class="form-control" name="admin_user" id="admin_user" value="<?=$result['username'];?>" required>
 		</div>
 	</div>
 	<div class="form-group">
@@ -134,7 +134,7 @@ echo "<option>", $row['domain'], "</option>";
 	<div class="form-group">
 		<label class="control-label col-sm-4" for="location">Location <small>(will be created if missing)</small>:</label>
 		<div class="col-sm-8">
-		<input type="text" class="form-control" name="location" id="location" value="<?php echo return_mailcow_config("backup_location"); ?>">
+		<input type="text" class="form-control" name="location" id="location" value="<?=return_mailcow_config("backup_location");?>">
 		</div>
 	</div>
 	<br /><br />
@@ -249,13 +249,13 @@ $srr_values = return_mailcow_config("srr");
 	<div class="form-group">
 		<label class="control-label col-sm-4" for="location">Folder name <small>(alphanumeric)</small>:</label>
 		<div class="col-sm-8">
-		<input type="text" class="form-control" name="public_folder_name" id="public_folder_name" value="<?php echo return_mailcow_config("public_folder_name"); ?>">
+		<input type="text" class="form-control" name="public_folder_name" id="public_folder_name" value="<?=return_mailcow_config("public_folder_name");?>">
 		</div>
 	</div>
 	<div class="form-group">
 		<div class="col-sm-offset-4 col-sm-8">
 			<div class="checkbox">
-			<label><input type="checkbox" name="use_public_folder" <?php echo return_mailcow_config("public_folder_status"); ?>> Enable public folder</label>
+			<label><input type="checkbox" name="use_public_folder" <?=return_mailcow_config("public_folder_status");?>> Enable public folder</label>
 			</div>
 			<small>Toggling this option does not delete mail in any public folder.</small>
 		</div>
@@ -263,7 +263,7 @@ $srr_values = return_mailcow_config("srr");
 	<div class="form-group">
 		<div class="col-sm-offset-4 col-sm-8">
 			<div class="checkbox">
-			<label><input type="checkbox" name="public_folder_pvt" <?php echo return_mailcow_config("public_folder_pvt"); ?>> Enable per-user seen flag</label>
+			<label><input type="checkbox" name="public_folder_pvt" <?=return_mailcow_config("public_folder_pvt");?>> Enable per-user seen flag</label>
 			</div>
 			<small>A "per-user seen flag"-enabled system will not mark a mail as read for User B, when User A has seen it, but User B did not.</small>
 		</div>
@@ -284,7 +284,7 @@ $srr_values = return_mailcow_config("srr");
 <form method="post">
 <div class="form-group">
 	<p>Provide a list of dangerous file types. Please take care of the formatting.</p>
-	<input class="form-control" type="text" id="ext" name="ext" value="<?php echo return_mailcow_config("extlist") ?>">
+	<input class="form-control" type="text" id="ext" name="ext" value="<?=return_mailcow_config("extlist");?>">
 	<p><pre>Format: ext1|ext1|ext3
 To scan all attachments: .*
 Enter "DISABLED" to disable this feature.</pre></p>
@@ -296,7 +296,7 @@ Enter "DISABLED" to disable this feature.</pre></p>
 	</div>
 	<div class="radio">
 		<label>
-		<input type="radio" name="vfilter" id="vfilter_scan_button" value="filter" <?php echo return_mailcow_config("vfilter") ?>>
+		<input type="radio" name="vfilter" id="vfilter_scan_button" value="filter" <?=return_mailcow_config("vfilter");?>>
 		Scan attachments with ClamAV and/or upload to VirusTotal
 		</label>
 	</div>
@@ -307,13 +307,13 @@ Enter "DISABLED" to disable this feature.</pre></p>
 			<h4>ClamAV</h4>
 			<div class="checkbox">
 					<label>
-					<input name="clamavenable" type="checkbox" <?php echo return_mailcow_config("cavenable") ?>>
+					<input name="clamavenable" type="checkbox" <?=return_mailcow_config("cavenable");?>>
 					Use ClamAV to scan mail
 					</label>
 			</div>
 			<p>
 			<ul class="nav nav-pills">
-				<li><a href="?av_dl">Download quarantined items<span class="badge"><?php echo_sys_info("positives"); ?></span></a></li>
+				<li><a href="?av_dl">Download quarantined items<span class="badge"><?php echo_sys_info("positives");?></span></a></li>
 			</ul></p>
 			<p>Clean directory <code>/opt/vfilter/clamav_positives/</code> to reset counter.</p>
 			<p>Senders of infected messages are informed about failed delivery.</p>
@@ -324,7 +324,7 @@ Enter "DISABLED" to disable this feature.</pre></p>
 			<h4>VirusTotal Uploader</h4>
 			<div class="checkbox">
 					<label>
-					<input name="virustotalenable" type="checkbox" <?php echo return_mailcow_config("vtenable") ?>>
+					<input name="virustotalenable" type="checkbox" <?=return_mailcow_config("vtenable");?>>
 					Use the "VirusTotal Uploader" feature
 					</label>
 			</div>
@@ -337,17 +337,17 @@ Enter "DISABLED" to disable this feature.</pre></p>
 			</p>
 			<div class="checkbox">
 					<label>
-					<input name="virustotalcheckonly" type="checkbox"  <?php echo return_mailcow_config("vtupload") ?>>
+					<input name="virustotalcheckonly" type="checkbox"  <?=return_mailcow_config("vtupload");?>>
 					Do <b>not</b> upload files to VirusTotal but check for a previous scan report. This also requires an API key!
 					</label>
 			</div>
 			<label for="vtapikey">VirusTotal API Key (<a href="https://www.virustotal.com/documentation/virustotal-community/#retrieve-api-key" target="_blank">?</a>)</label>
-			<p><input class="form-control" id="vtapikey" type="text" name="vtapikey" placeholder="64 characters, alphanumeric" pattern="[a-zA-Z0-9]{64}" value="<?php echo return_mailcow_config("vtapikey"); ?>"></p>
+			<p><input class="form-control" id="vtapikey" type="text" name="vtapikey" placeholder="64 characters, alphanumeric" pattern="[a-zA-Z0-9]{64}" value="<?=return_mailcow_config("vtapikey");?>"></p>
 			</small>
 		</div>
 		<div class="col-sm-12">
 		<h4>Filter Log (newest)</h4>
-		<p><pre><?php echo_sys_info("vfilterlog", "20"); ?></pre></p>
+		<p><pre><?php echo_sys_info("vfilterlog", "20");?></pre></p>
 		</div>
 	</div>
 	<br /><button type="submit" class="btn btn-default btn-raised btn-sm">Apply</button>
@@ -378,7 +378,7 @@ Enter "DISABLED" to disable this feature.</pre></p>
 	<div class="checkbox">
 	<label>
 	<input type="hidden" name="anonymize_">
-	<input name="anonymize" type="checkbox" <?php echo return_mailcow_config("anonymize") ?>>
+	<input name="anonymize" type="checkbox" <?=return_mailcow_config("anonymize");?>>
 		Anonymize outgoing mail
 	</label>
 	</div>
@@ -419,7 +419,7 @@ Enter "DISABLED" to disable this feature.</pre></p>
 <div class="panel-heading">Message Size</div>
 <div class="panel-body">
 	<form class="form-inline" method="post">
-	<p>Current message size limitation: <strong><?php echo return_mailcow_config("maxmsgsize"); ?>MB</strong></p>
+	<p>Current message size limitation: <strong><?=return_mailcow_config("maxmsgsize");?>MB</strong></p>
 	<p>This changes values in PHP, Nginx and Postfix. Services will be reloaded.</p>
 	<div class="form-group">
 		<input type="number" class="form-control" id="maxmsgsize" name="maxmsgsize" placeholder="in MB" min="1" max="250">
@@ -449,18 +449,18 @@ Enter "DISABLED" to disable this feature.</pre></p>
 ; ================
 
 [...]
-_imaps._tcp         IN SRV     0 1 993 <?php echo $MYHOSTNAME; ?>.
-_imap._tcp          IN SRV     0 1 143 <?php echo $MYHOSTNAME; ?>.
-_submission._tcp    IN SRV     0 1 587 <?php echo $MYHOSTNAME; ?>.
-@                   IN MX 10   <?php echo $MYHOSTNAME_0, "\n"; ?>
+_imaps._tcp         IN SRV     0 1 993 <?=$MYHOSTNAME;?>.
+_imap._tcp          IN SRV     0 1 143 <?=$MYHOSTNAME;?>.
+_submission._tcp    IN SRV     0 1 587 <?=$MYHOSTNAME;?>.
+@                   IN MX 10   <?=$MYHOSTNAME_0, "\n";?>
 @                   IN TXT     "v=spf1 mx -all"
-autoconfig          IN A       <?php echo $IP, "\n"; ?>
-dav                 IN A       <?php echo $IP, "\n"; ?>
-<?php echo str_pad($MYHOSTNAME_0, 20); ?>IN A       <?php echo $IP, "\n"; ?>
+autoconfig          IN A       <?=$IP, "\n";?>
+<?=str_pad($DAV_SUBDOMAIN, 20);?>IN A       <?=$IP, "\n";?>
+<?=str_pad($MYHOSTNAME_0, 20);?>IN A       <?=$IP, "\n";?>
 
 ; !!!!!!!!!!!!!!!!
 ; Do not forget to set a PTR record in your Reverse DNS configuration!
-; Your IPs PTR should point to <?php echo $MYHOSTNAME, "\n"; ?>
+; Your IPs PTR should point to <?=$MYHOSTNAME, "\n";?>
 ; !!!!!!!!!!!!!!!!
 </pre>
 </div>
@@ -499,10 +499,10 @@ nano /opt/vfilter/replies
 ; mailcow comes with plugins helping to export Cal- and CardDAV data to .vcf and .ics files.
 ; Each user can export data he has access to.
 ; You can generate these exports by finding a url to your calendar, and adding ?export at the end of the url. This will automatically trigger a download:
-https://<?php echo $DAV_SUBDOMAIN.$MYHOSTNAME_1.$MYHOSTNAME_2; ?>/calendars/you@domain.tld/default?export
+https://<?=$DAV_SUBDOMAIN.$MYHOSTNAME_1.$MYHOSTNAME_2;?>/calendars/you@domain.tld/default?export
 
 ; The same procedure for address books:
-https://<?php echo $DAV_SUBDOMAIN.$MYHOSTNAME_1.$MYHOSTNAME_2; ?>/addressbooks/you@domain.tld/default?export
+https://<?=$DAV_SUBDOMAIN.$MYHOSTNAME_1.$MYHOSTNAME_2;?>/addressbooks/you@domain.tld/default?export
 
 ; Please use a Cal-/CardDAV client of your choice to find out the URI of self-created calendars and address books.
 ; Administrators can use MySQL to find a users calendar and address book URI:
@@ -531,25 +531,25 @@ mysql --defaults-file=/etc/mysql/debian.cnf mailcow_database_name -e "SELECT uri
 <p>This is a very simple system information function. Please be aware that a high RAM usage is what you want on a server.</p>
 <div class="row">
 	<div class="col-md-6">
-		<h4>Disk usage (/var/vmail) - <?php echo_sys_info("maildisk"); ?>%</h4>
+		<h4>Disk usage (/var/vmail) - <?php echo_sys_info("maildisk");?>%</h4>
 		<div class="progress">
-		  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo_sys_info("maildisk"); ?>"
-		  aria-valuemin="0" aria-valuemax="100" style="width:<?php echo_sys_info("maildisk"); ?>%">
+		  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo_sys_info("maildisk");?>"
+		  aria-valuemin="0" aria-valuemax="100" style="width:<?php echo_sys_info("maildisk");?>%">
 		  </div>
 		</div>
 	</div>
 	<div class="col-md-6">
-		<h4>RAM usage - <?php echo_sys_info("ram"); ?>%</h4>
+		<h4>RAM usage - <?php echo_sys_info("ram");?>%</h4>
 		<div class="progress">
-		  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo_sys_info("ram"); ?>"
-		  aria-valuemin="0" aria-valuemax="100" style="width:<?php echo_sys_info("ram"); ?>%">
+		  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo_sys_info("ram");?>"
+		  aria-valuemin="0" aria-valuemax="100" style="width:<?php echo_sys_info("ram");?>%">
 		  </div>
 		</div>
 	</div>
 </div>
 <h4>Mail queue</h4>
 <pre>
-<?php echo_sys_info("mailq"); ?>
+<?php echo_sys_info("mailq");?>
 </pre>
 </div>
 </div>
