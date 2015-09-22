f73691e1b4ece56030334b51e5cf9442c38c070c
andryyy <andre.peters@debinux.de>
2015-09-22 13:27:09 +0200

    fixes #118

--- install.sh
+++ install.sh
@@ -8,16 +8,17 @@ cat includes/banner
 source includes/versions
 source includes/functions.sh
 
-case $1 in
+case ${1} in
 	"-u" | "--upgrade" | "-uu" | "--upgrade-unattended" )
-		[[ $1 == "-uu" || $1 == "--upgrade-unattended" ]] && inst_unattended="yes"
+		[[ ${1} == "-uu" || ${1} == "--upgrade-unattended" ]] && inst_unattended="yes"
 		upgradetask
-		echo $mailcow_version > /etc/mailcow_version
+		echo ${mailcow_version} > /etc/mailcow_version
 echo --------------------------------- >> installer.log
-echo UPGRADE to $mailcow_version on $(date) >> installer.log
+echo UPGRADE to ${mailcow_version} on $(date) >> installer.log
 echo --------------------------------- >> installer.log
-echo Fail2ban version: $fail2ban_version >> installer.log
-echo Roundcube version: $roundcube_version >> installer.log
+echo Fail2ban version: ${fail2ban_version} >> installer.log
+echo Roundcube version: ${roundcube_version} >> installer.log
+echo FuGlu version: ${fuglu_version}
 echo --------------------------------- >> installer.log
 		exit 0
 		;;
@@ -32,10 +33,10 @@ checksystem
 checkports
 checkconfig
 
-echo "    $(textb "Hostname")            $sys_hostname
-    $(textb "Domain")              $sys_domain
-    $(textb "FQDN")                $sys_hostname.$sys_domain
-    $(textb "Timezone")            $sys_timezone
+echo "    $(textb "Hostname")            ${sys_hostname}
+    $(textb "Domain")              ${sys_domain}
+    $(textb "FQDN")                ${sys_hostname}.$sys_domain}
+    $(textb "Timezone")            ${sys_timezone}
     $(textb "mailcow MySQL")       ${my_mailcowuser}:${my_mailcowpass}@${my_dbhost}/${my_mailcowdb}
     $(textb "Roundcube MySQL")     ${my_rcuser}:${my_rcpass}@${my_dbhost}/${my_rcdb}
     $(textb "mailcow admin user")  ${mailcow_admin_user}
@@ -44,33 +45,35 @@ echo "    $(textb "Hostname")            $sys_hostname
 returnwait "Reading configuration" "System environment"
 
 echo --------------------------------- > installer.log
-echo MySQL database host: $my_dbhost  >> installer.log
+echo MySQL database host: ${my_dbhost}  >> installer.log
 echo --------------------------------- >> installer.log
-echo Use existing database: $my_useexisting  >> installer.log
+echo Use existing database: ${my_useexisting}  >> installer.log
 echo --------------------------------- >> installer.log
-echo MySQL mailcow database: $my_mailcowdb >> installer.log
-echo MySQL mailcow username: $my_mailcowuser >> installer.log
-echo MySQL mailcow password: $my_mailcowpass >> installer.log
+echo MySQL mailcow database: ${my_mailcowdb} >> installer.log
+echo MySQL mailcow username: ${my_mailcowuser} >> installer.log
+echo MySQL mailcow password: ${my_mailcowpass} >> installer.log
 echo --------------------------------- >> installer.log
-echo MySQL Roundcube database: $my_rcdb >> installer.log
-echo MySQL Roundcube username: $my_rcuser >> installer.log
-echo MySQL Roundcube password: $my_rcpass >> installer.log
+echo MySQL Roundcube database: ${my_rcdb} >> installer.log
+echo MySQL Roundcube username: ${my_rcuser} >> installer.log
+echo MySQL Roundcube password: ${my_rcpass} >> installer.log
 echo --------------------------------- >> installer.log
 echo Only set when MySQL was not available >> installer.log
-echo MySQL root password: $my_rootpw >> installer.log
+echo MySQL root password: ${my_rootpw} >> installer.log
 echo --------------------------------- >> installer.log
 echo mailcow administrator >> installer.log
-echo Username: $mailcow_admin_user >> installer.log
-echo Password: $mailcow_admin_pass >> installer.log
+echo Username: ${mailcow_admin_user} >> installer.log
+echo Password: ${mailcow_admin_pass} >> installer.log
 echo --------------------------------- >> installer.log
-echo FQDN: $sys_hostname.$sys_domain >> installer.log
-echo Timezone: $sys_timezone >> installer.log
+echo FQDN: ${sys_hostname}.${sys_domain} >> installer.log
+echo Timezone: ${sys_timezone} >> installer.log
 echo --------------------------------- >> installer.log
 echo Web root: https://${sys_hostname}.${sys_domain} >> installer.log
 echo DAV web root: https://${httpd_dav_subdomain}.${sys_domain} >> installer.log
 echo --------------------------------- >> installer.log
 echo Fail2ban version: $fail2ban_version >> installer.log
 echo Roundcube version: $roundcube_version >> installer.log
+echo FuGlu version: ${fuglu_version} >> installer.log
+echo mailcow version: ${mailcow_version} >> installer.log
 echo --------------------------------- >> installer.log
 
 installtask environment
@@ -121,7 +124,7 @@ returnwait "Restarting services" "Checking DNS settings"
 installtask checkdns
 returnwait "Checking DNS settings" "Finish installation"
 
-echo $mailcow_version > /etc/mailcow_version
+echo ${mailcow_version} > /etc/mailcow_version
 chmod 600 installer.log
 echo
 echo "`tput setaf 2`Finished installation`tput sgr0`"
