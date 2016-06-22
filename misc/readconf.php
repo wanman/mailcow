a8184e2c18027e744ba7578017328d0f4965d709
andryyy <andre.peters@debinux.de>
2016-06-22 08:30:08 +0200

    Remove DES key detection

--- misc/readconf.php
+++ misc/readconf.php
@@ -10,7 +10,6 @@ echo $database_name, PHP_EOL;
 }
 if (file_exists("/var/www/mail/rc/config/config.inc.php")){
 include "/var/www/mail/rc/config/config.inc.php";
-echo $config["des_key"], PHP_EOL;
 echo parse_url($config["db_dsnw"])[user], PHP_EOL;
 echo parse_url($config["db_dsnw"])[pass], PHP_EOL;
 echo substr(parse_url($config["db_dsnw"])[path], 1), PHP_EOL;
