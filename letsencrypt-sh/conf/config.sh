37ba1a7f315837926c3e02da93fa54940e997d80
andryyy <andre.peters@debinux.de>
2016-08-08 10:09:13 +0200

    Fix agreement for LE

--- letsencrypt-sh/conf/config.sh
+++ letsencrypt-sh/conf/config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CA="https://acme-v01.api.letsencrypt.org/directory"
+LICENSE="https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf"
 CHALLENGETYPE="http-01"
 BASEDIR="/etc/ssl/mail"
 WELLKNOWN="/var/www/mail/.well-known/acme-challenge"
