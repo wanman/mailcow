99fa8eb9adc2e525ee4262afecca391497a587b5
andryyy <andre.peters@debinux.de>
2016-09-28 22:05:43 +0200

    Fixes for nginx as

--- webserver/htdocs/mail/autodiscover.php
+++ webserver/htdocs/mail/autodiscover.php
@@ -21,7 +21,7 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'Outlook')) {
         $config['autodiscoverType'] = 'imap';
 }
 ?>
-<?xml version="1.0" encoding="utf-8" ?>
+<?php echo '<?xml version="1.0" encoding="utf-8" ?>'; ?>
 <Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">
 <?php
 $data = file_get_contents("php://input");
