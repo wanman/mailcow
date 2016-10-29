d3ab20dea087116ef7edaeeca7686986f39e2d2e
Phoenix Eve Aspacio <aspaciop@gmail.com>
2016-10-29 21:48:41 +0800

    Added Portuguese Translation
    This pull request is in favour of Pull Request #572

--- webserver/htdocs/mail/lang/lang.pt.php
+++ webserver/htdocs/mail/lang/lang.pt.php
@@ -191,6 +191,12 @@ $lang['mailbox']['remove'] = 'Remover';
 $lang['mailbox']['edit'] = 'Alterar';
 $lang['mailbox']['view'] = 'Abrir';
 $lang['mailbox']['archive'] = 'Arquivo';
+$lang['mailbox']['no_record'] = 'No Record';
+$lang['mailbox']['add_domain'] = 'Adicionar domínio';
+$lang['mailbox']['add_domain_alias'] = 'Adicionar Domínio Alias';
+$lang['mailbox']['add_mailbox'] = 'Adicionar Mailbox';
+$lang['mailbox']['add_alias'] = 'Adicionar Alias';
+
 $lang['info']['no_action'] = 'Nenhuma ação foi definida';
 $lang['delete']['title'] = 'Remover objeto';
 $lang['delete']['remove_domain_warning'] = '<b>Aviso:</b> Você está prestes a remover o Domínio <b>%s</b>!';
