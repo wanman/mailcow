7ad9d94f94c0195ab3c15515f6a7a56d44e65680
Phoenix Eve Aspacio <aspaciop@gmail.com>
2016-11-05 18:04:03 +0800

    An img tag should have alt tag in most cases

--- webserver/htdocs/mail/inc/header.inc.php
+++ webserver/htdocs/mail/inc/header.inc.php
@@ -179,7 +179,7 @@ endif;
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/"><img style="margin-top:-5px;" src="/img/xs_mailcow.png" /></a>
+			<a class="navbar-brand" href="/"><img alt="mailcow-logo" style="margin-top:-5px;" src="/img/xs_mailcow.png" /></a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
